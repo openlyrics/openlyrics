ddf3b928d03fbc7a22b226de8434d51a6bd4b08d
Martin Zibricky <matysek@users.noreply.github.com>
2010-01-21 12:56:38 +0000

    [openlyrics] update copyright year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'OpenLyrics'
-copyright = u'2009, Martin Zibricky, Raoul Snyman'
+copyright = u'2009-2010, Martin Zibricky, Raoul Snyman'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
