3ae96e0cd6aa457c8ba7d4cc1547e9ac3cba2356
Martin Zibricky <matysek@users.noreply.github.com>
2010-03-19 16:09:25 +0000

    [openlyrics] update doc for v0.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ project = u'OpenLyrics'
 copyright = u'2009-2010, Martin Zibricky, Raoul Snyman'
 
 # The short X.Y version.
-version = '0.6'
+version = '0.7'
 # The full version, including alpha/beta/rc tags.
-release = '0.6'
+release = '0.7'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
