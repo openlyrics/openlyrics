30761b1c69a1326c884560a9375a4af45a08ae55
Martin Zibricky <matysek@users.noreply.github.com>
2010-06-03 22:14:11 +0200

    pylib: add version of openlyrics python library

--- lib/python/openlyrics.py
+++ lib/python/openlyrics.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+__version__ = '0.1'
+
 '''
 Provides a module to access OpenLyrics data type.
 
