835e146a69c20f2a04bcd6541c0404d1e9e3584a
John Zaitseff <J.Zaitseff@zap.org.au>
2012-04-10 21:27:20 +1000

    Removed trailing whitespace (trivial).

--- tools/opensong2openlyrics.py
+++ tools/opensong2openlyrics.py
@@ -86,95 +86,95 @@ for item in open(CCLITHEMES_FILE, 'r').readlines():
 
 # try to map OpenSong theme to ccli
 def map_to_ccli_themes(name):
-    if name == "Christ: Attributes" : 
+    if name == "Christ: Attributes" :
         map = ['Christ']
-    elif name == "Christ: Birth" : 
+    elif name == "Christ: Birth" :
         map = ['Seasonal / Christmas']
-    elif name == "Christ: Death/Atonement" : 
+    elif name == "Christ: Death/Atonement" :
         map = ['Atonement']
-    elif name == "Christ: Power/Majesty" : 
+    elif name == "Christ: Power/Majesty" :
         map = ["God's Attributes / Power", "God's Attributes / Majesty"]
-    elif name == "Christ: Love/Mercy" : 
+    elif name == "Christ: Love/Mercy" :
         map = ['Love', 'Mercy']
-    elif name == "Christ: Resurrection" : 
+    elif name == "Christ: Resurrection" :
         map = ['Resurrection']
-    elif name == "Christ: Second Coming" : 
+    elif name == "Christ: Second Coming" :
         map = ['Second Coming']
-    elif name == "Christ: Victory" : 
+    elif name == "Christ: Victory" :
         map = ['Victory']
-    elif name == "Church: Commitment/Obedience" : 
+    elif name == "Church: Commitment/Obedience" :
         map = ['Commitment', 'Obedience']
-    elif name == "Church: Country" : 
+    elif name == "Church: Country" :
         map = ['Country']
-    elif name == "Church: Eternal Life/Heaven" : 
+    elif name == "Church: Eternal Life/Heaven" :
         map = ['Eternal Life', 'Heaven']
-    elif name == "Church: Evangelism" : 
+    elif name == "Church: Evangelism" :
         map = ['Evangelism']
-    elif name == "Church: Family/Fellowship" : 
+    elif name == "Church: Family/Fellowship" :
         map = ['Family', 'Fellowship']
-    elif name == "Church: Fellowship w/ God" : 
+    elif name == "Church: Fellowship w/ God" :
         map = ['Fellowship']
-    elif name == "Church: Purity/Holiness" : 
+    elif name == "Church: Purity/Holiness" :
         map = ['Cleansing', 'Holiness']
-    elif name == "Church: Repentance/Salvation" : 
+    elif name == "Church: Repentance/Salvation" :
         map = ['Repentance', 'Salvation']
-    elif name == "Church: Renewal" : 
+    elif name == "Church: Renewal" :
         map = ['Renewal']
-    elif name == "Church: Service/Ministry" : 
+    elif name == "Church: Service/Ministry" :
         map = ['Service ']
-    elif name == "Church: Spiritual Hunger" : 
+    elif name == "Church: Spiritual Hunger" :
         map = ['Spiritual Hunger']
-    elif name == "Fruit: Faith/Hope" : 
+    elif name == "Fruit: Faith/Hope" :
         map = ['Faith', 'Hope']
-    elif name == "Fruit: Love" : 
+    elif name == "Fruit: Love" :
         map = ['Love']
-    elif name == "Fruit: Joy" : 
+    elif name == "Fruit: Joy" :
         map = ['Joy']
-    elif name == "Fruit: Peace/Comfort" : 
+    elif name == "Fruit: Peace/Comfort" :
         map = ['Peace']
-    elif name == "Fruit: Patience/Kindness" : 
+    elif name == "Fruit: Patience/Kindness" :
         map = ['Patience', 'Kindness']
-    elif name == "Fruit: Humility/Meekness" : 
+    elif name == "Fruit: Humility/Meekness" :
         map = ['Humility']
-    elif name == "God: Attributes" : 
+    elif name == "God: Attributes" :
         map = ["God's Attributes"]
-    elif name == "God: Creator/Creation" : 
+    elif name == "God: Creator/Creation" :
         map = ['Creator', 'Creation']
-    elif name == "God: Father" : 
+    elif name == "God: Father" :
         map = ["God's Attributes / Father"]
-    elif name == "God: Guidance/Care" : 
+    elif name == "God: Guidance/Care" :
         map = ['Guidance', 'Care']
-    elif name == "God: Holy Spirit" : 
+    elif name == "God: Holy Spirit" :
         map = ['Holy Spirit']
-    elif name == "God: Holiness" : 
+    elif name == "God: Holiness" :
         map = ['Holiness']
-    elif name == "God: Love/Mercy" : 
+    elif name == "God: Love/Mercy" :
         map = ["God's Attributes / Love", "God's Attributes / Mercy"]
-    elif name == "God: Power/Majesty" : 
+    elif name == "God: Power/Majesty" :
         map = ["God's Attributes / Power", "God's Attributes / Majesty"]
-    elif name == "God: Promises" : 
+    elif name == "God: Promises" :
         map = ['Promise']
-    elif name == "God: Victory" : 
+    elif name == "God: Victory" :
         map = ['Victory']
-    elif name == "God: Word" : 
+    elif name == "God: Word" :
         map = ["God's Word"]
-    elif name == "Worship: Assurance/Trust" : 
+    elif name == "Worship: Assurance/Trust" :
         map = ['Assurance', 'Trust']
-    elif name == "Worship: Call/Opening" : 
+    elif name == "Worship: Call/Opening" :
         map = ['Call', 'Opening']
-    elif name == "Worship: Celebration" : 
+    elif name == "Worship: Celebration" :
         map = ['Celebration']
-    elif name == "Worship: Declaration" : 
+    elif name == "Worship: Declaration" :
         map = ['Declaration']
-    elif name == "Worship: Intimacy" : 
+    elif name == "Worship: Intimacy" :
         map = ['Intimacy']
-    elif name == "Worship: Invitation" : 
+    elif name == "Worship: Invitation" :
         map = ['Invitation']
-    elif name == "Worship: Praise/Adoration" : 
+    elif name == "Worship: Praise/Adoration" :
         map = ['Praise', 'Adoration ']
-    elif name == "Worship: Prayer/Devotion" : 
+    elif name == "Worship: Prayer/Devotion" :
         map = ['Prayer', 'Devotion']
-    elif name == "Worship: Provision/Deliverance" : 
+    elif name == "Worship: Provision/Deliverance" :
         map = ['Provision', 'Deliverance']
     elif name == "Worship: Thankfulness":
         map = ['Thankfulness']
@@ -195,7 +195,7 @@ class LyricsParser(object):
     def parse(self, os_lyrics):
         tree = etree.Element('lyrics')
         lines = os_lyrics.splitlines()
-        
+
         # line with chords (line starts with '.'
         linechords = None
 
@@ -268,7 +268,7 @@ class LyricsParser(object):
             elem.set('name', chords[key])
 
         #print(etree.tostring(root))
-            
+
         # split text to snippets
         snippets = []
         id1 = 0
@@ -318,7 +318,7 @@ class LyricsParser(object):
                 chordlines[i] = chords[offset : offset+length]
                 offset += length + 1
                 #print(chordlines[i])
-        
+
         #print(chordlines)
 
         # create element <lines> if necessary
@@ -327,7 +327,7 @@ class LyricsParser(object):
             lines_tree = etree.SubElement(last_verse_tree, 'lines')
         else:
             lines_tree = last_verse_tree[-1] # last subelement <lines>
-        
+
         # parse lines and create xml structure
         for i in range(len(texts)):
             line_tree = self._parse_line(texts[i], chordlines[i])
@@ -343,7 +343,7 @@ class OpenLyricsConverter(object):
         self.olyrics = etree.ElementTree(etree.Element('song'))
 
     # public API
-        
+
     def convert(self):
         print('Converting...')
         osong = self.osong.getroot()
@@ -369,7 +369,7 @@ class OpenLyricsConverter(object):
         self._copy_subelement(osong, 'user3', olprop, 'comments', 'comment')
 
 
-        # other 
+        # other
         self._conv_lyrics(osong, olyrics)
         self._set_metadata(olyrics)
 
@@ -391,7 +391,7 @@ class OpenLyricsConverter(object):
         '''
         Add content from element in one tree into element
         in another tree. Element in another tree are created.
-        
+
         return  tree element with copied content or None when element not
                 present or empty content
         '''
@@ -409,7 +409,7 @@ class OpenLyricsConverter(object):
         Add content from element in one tree into subelement
         in another tree. Element and/or subelement in another
         tree are created.
-        
+
         return  etree element with copied content or None when element not
                 present or empty content
         '''
@@ -434,7 +434,7 @@ class OpenLyricsConverter(object):
         text = text.text
         if not text:
             return
-        
+
         text = text.strip()
         authors = []
         # more names are usually separated by ',', ';', '&' or 'and'
@@ -449,7 +449,7 @@ class OpenLyricsConverter(object):
         for author in authors:
             subelem = etree.SubElement(elem, 'author')
             subelem.text = author
-        
+
 
 
     def _conv_verseorder(self, os_tree, os_elem, ol_tree, ol_elem):
@@ -506,7 +506,7 @@ class OpenLyricsConverter(object):
                         elem.set('id', unicode(id + 1)) # indexing beginns with '1'
                     except ValueError:
                         pass
-                    
+
 
     def _conv_lyrics(self, os_tree, ol_tree):
         text = os_tree.find('lyrics').text
@@ -524,13 +524,13 @@ class OpenLyricsConverter(object):
         ol_root.set('modifiedDate', datetime.utcnow().isoformat())
 
 
-def main(): 
-    
+def main():
+
     if len(sys.argv) != 3:
         print('Usage:')
         print('  python %s opensong_file  openlyrics_file.xml' % __file__)
         exit(1)
-    
+
     else:
         opensong_file = sys.argv[1]
         openlyrics_file = sys.argv[2]
